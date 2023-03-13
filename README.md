12a8851b456cc1781c050edcacb83cc58cffe2e4
AlainCartaya <alaincs40@gmail.com>
2023-03-13 15:37:56 -0400

    Add quasar table example

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # School-timetabling
 
 * run `yarn` after clone
+
+* you can test on `http://localhost:9000/#/timetabling/`
---
+++ src/pages/TablingPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <tabling-component />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TablingComponent from 'components/TablingComponent.vue';
+
+export default defineComponent({
+  name: 'TablingPage',
+  components: { TablingComponent },
+  setup() {
+    return {};
+  },
+});
+</script>
