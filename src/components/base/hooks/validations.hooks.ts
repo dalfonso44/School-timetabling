784eaea39426338736ae4550fe75d0f72b2a3cd3
dalfonso44 <diamisap2000@gmail.com>
2023-09-24 17:05:12 -0400

    Add notify

--- src/components/base/hooks/validations.hooks.ts
+++ src/components/base/hooks/validations.hooks.ts
@@ -1,10 +1,16 @@
 import { BaseSchedule } from '../models/basic';
+import { Notify } from 'quasar';
+
 
 export const twoSubjectInSameRoom = (
   subjects: array,
   baseSchedule: BaseSchedule
 ) => {
   if (subjects[0][`subject`] != baseSchedule.subject){
+    Notify.create({
+      type: 'negative',
+      message: `No se puede impartir ${subjects[0][`subject`]} y ${baseSchedule.subject} al mismo tiempo`
+    });
     return false;
   }
   return true;
@@ -16,6 +22,10 @@ export const classType = (
   baseSchedule: BaseSchedule
 ) => {
   if (subjects[0][`cp`] != baseSchedule.cp){
+    Notify.create({
+      type: 'negative',
+      message: `No se puede impartir una conferencia y una clase práctica al mismo tiempo`
+    });
     return false;
   }
   return true;
