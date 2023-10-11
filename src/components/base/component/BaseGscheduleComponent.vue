de3facb4be3500dd1027a7b657f7ebb5f120c083
Diamis Alfonso <91924607+dalfonso44@users.noreply.github.com>
2023-10-11 18:57:28 -0400

    13 use the keyboard to move around the tables (#31)
    * first iter over move on input
    
    * add ref grid system for moving 4 direction in the input table
    
    ---------
    
    Co-authored-by: AlainCartaya <alaincs40@gmail.com>

--- src/components/base/component/BaseGscheduleComponent.vue
+++ src/components/base/component/BaseGscheduleComponent.vue
@@ -272,7 +272,7 @@
 
           <q-td
             :props="props"
-            v-for="item in sch.config.daysOptions"
+            v-for="(item, j) in sch.config.daysOptions"
             :key="item"
             :class="{
               'bg-grey-3': props.rowIndex == 3,
@@ -283,6 +283,7 @@
               !!props.row[`${item}_custom_color`] &&
               `background-color: ${props.row[`${item}_custom_color`]}`
             "
+            @keyup.ctrl.stop="onkeydown"
             style="padding-bottom: 0 !important"
             @click="!editing && onPaint(props.rowIndex, item)"
           >
@@ -290,6 +291,9 @@
               v-if="props.rowIndex != 3 && !readonly"
               type="text"
               dense
+              :ref="(field) => (fields[props.rowIndex][j] = field)"
+              :key="`${props.rowIndex}${item}`"
+              @focus="onfocus(props.rowIndex, j)"
               :readonly="!editing"
               borderless
               hide-bottom-space
@@ -419,7 +423,16 @@ export default {
       return true;
     };
 
+    const fields = ref(
+      props.school_data[props.selected_group].map(() =>
+        props.sch.config.daysOptions.map(() => null)
+      )
+    );
+    const indexFocus = ref([0, 0]);
+    console.log('>>>|', fields);
     return {
+      fields,
+      indexFocus,
       alert: ref(false),
       card: ref(false),
       editing,
@@ -429,6 +442,51 @@ export default {
       show_new_group: showNewGroup,
       columns,
 
+      onkeydown(event: any) {
+        console.log('>>>', event);
+        console.log(':::', fields.value);
+        const n = props.school_data[props.selected_group].length;
+        const m = props.sch.config.daysOptions.length;
+        if (
+          event.code === 'ArrowRight' &&
+          !(indexFocus.value[0] == n - 1 && indexFocus.value[1] == m - 1)
+        ) {
+          indexFocus.value[1] += 1;
+        }
+        if (
+          event.code === 'ArrowLeft' &&
+          !(indexFocus.value[0] == 0 && indexFocus.value[1] == 0)
+        ) {
+          indexFocus.value[1] -= 1;
+        }
+        if (indexFocus.value[1] >= m) {
+          indexFocus.value[0] += 1;
+          indexFocus.value[1] = 0;
+        }
+        if (indexFocus.value[1] < 0) {
+          indexFocus.value[0] -= 1;
+          indexFocus.value[1] = m - 1;
+        }
+        if (indexFocus.value[0] == 3) {
+          if (indexFocus.value[1] == 0) indexFocus.value[0] += 1;
+          else indexFocus.value[0] -= 1;
+        }
+        if (event.code === 'ArrowUp' && !(indexFocus.value[0] == 0)) {
+          indexFocus.value[0] -= 1;
+          if (indexFocus.value[0] == 3) indexFocus.value[0] -= 1;
+        }
+        if (event.code === 'ArrowDown' && !(indexFocus.value[0] == n - 1)) {
+          indexFocus.value[0] += 1;
+          if (indexFocus.value[0] == 3) indexFocus.value[0] += 1;
+        }
+        if (indexFocus.value)
+          fields.value[indexFocus.value[0]][indexFocus.value[1]]?.focus();
+      },
+      onfocus(i: number, j: number) {
+        console.log('>>>', i, j);
+        indexFocus.value[0] = i;
+        indexFocus.value[1] = j;
+      },
       onSave() {
         emit('on-save');
         // timeTableData.saveData(rows.value);
