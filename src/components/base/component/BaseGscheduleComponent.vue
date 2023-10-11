1ebe27133450230146afae552361f2000e1a51cd
Diamis Alfonso <91924607+dalfonso44@users.noreply.github.com>
2023-10-11 18:57:28 -0400

    13 use the keyboard to move around the tables (#31)
    * first iter over move on input
    
    * add ref grid system for moving 4 direction in the input table
    
    ---------
    
    Co-authored-by: AlainCartaya <alaincs40@gmail.com>

--- src/components/base/component/BaseGscheduleComponent.vue
+++ src/components/base/component/BaseGscheduleComponent.vue
@@ -226,7 +226,7 @@
 
           <q-td
             :props="props"
-            v-for="item in sch.config.daysOptions"
+            v-for="(item, j) in sch.config.daysOptions"
             :key="item"
             :class="{
               'bg-grey-3': props.rowIndex == 3,
@@ -237,6 +237,7 @@
               !!props.row[`${item}_custom_color`] &&
               `background-color: ${props.row[`${item}_custom_color`]}`
             "
+            @keyup.ctrl.stop="onkeydown"
             style="padding-bottom: 0 !important"
             @click="!editing && onPaint(props.rowIndex, item)"
           >
@@ -244,6 +245,9 @@
               v-if="props.rowIndex != 3"
               type="text"
               dense
+              :ref="(field) => (fields[props.rowIndex][j] = field)"
+              :key="`${props.rowIndex}${item}`"
+              @focus="onfocus(props.rowIndex, j)"
               :readonly="!editing"
               borderless
               hide-bottom-space
@@ -287,13 +291,9 @@ import { ref } from 'vue';
 import { QTableColumn } from 'quasar';
 import { getColor } from '../hooks/utils.hooks';
 
-
-
-
-
 export default {
   props: {
-    sch:{
+    sch: {
       type: Object,
       required: true
     },
@@ -343,11 +343,15 @@ export default {
     const editing = ref(true);
     const importFile = ref(null);
 
-    const columns: QTableColumn[]=[
+    const columns: QTableColumn[] = [
       { name: 'turn', align: 'center', field: 'turn', label: '' },
-      ...props.sch.config.daysOptions.map((day:string)=>({name: day, align:'center', field:day, label: day}))
-
-    ]
+      ...props.sch.config.daysOptions.map((day: string) => ({
+        name: day,
+        align: 'center',
+        field: day,
+        label: day
+      }))
+    ];
 
     const verifyVerbose = (value: string) => {
       if (!value) return true;
@@ -357,7 +361,16 @@ export default {
       return true;
     };
 
+    const fields = ref(
+      props.school_data[props.selected_group].map(() =>
+        props.sch.config.daysOptions.map(() => null)
+      )
+    );
+    const indexFocus = ref([0, 0]);
+    console.log('>>>|', fields);
     return {
+      fields,
+      indexFocus,
       alert: ref(false),
       card: ref(false),
       editing,
@@ -366,7 +379,51 @@ export default {
       show_new_year: showNewTime,
       show_new_group: showNewGroup,
       columns,
-     
+      onkeydown(event: any) {
+        console.log('>>>', event);
+        console.log(':::', fields.value);
+        const n = props.school_data[props.selected_group].length;
+        const m = props.sch.config.daysOptions.length;
+        if (
+          event.code === 'ArrowRight' &&
+          !(indexFocus.value[0] == n - 1 && indexFocus.value[1] == m - 1)
+        ) {
+          indexFocus.value[1] += 1;
+        }
+        if (
+          event.code === 'ArrowLeft' &&
+          !(indexFocus.value[0] == 0 && indexFocus.value[1] == 0)
+        ) {
+          indexFocus.value[1] -= 1;
+        }
+        if (indexFocus.value[1] >= m) {
+          indexFocus.value[0] += 1;
+          indexFocus.value[1] = 0;
+        }
+        if (indexFocus.value[1] < 0) {
+          indexFocus.value[0] -= 1;
+          indexFocus.value[1] = m - 1;
+        }
+        if (indexFocus.value[0] == 3) {
+          if (indexFocus.value[1] == 0) indexFocus.value[0] += 1;
+          else indexFocus.value[0] -= 1;
+        }
+        if (event.code === 'ArrowUp' && !(indexFocus.value[0] == 0)) {
+          indexFocus.value[0] -= 1;
+          if (indexFocus.value[0] == 3) indexFocus.value[0] -= 1;
+        }
+        if (event.code === 'ArrowDown' && !(indexFocus.value[0] == n - 1)) {
+          indexFocus.value[0] += 1;
+          if (indexFocus.value[0] == 3) indexFocus.value[0] += 1;
+        }
+        if (indexFocus.value)
+          fields.value[indexFocus.value[0]][indexFocus.value[1]]?.focus();
+      },
+      onfocus(i: number, j: number) {
+        console.log('>>>', i, j);
+        indexFocus.value[0] = i;
+        indexFocus.value[1] = j;
+      },
       onSave() {
         emit('on-save');
         // timeTableData.saveData(rows.value);
