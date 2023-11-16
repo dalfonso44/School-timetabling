aceb0a6a0fc3a203224fcb167d4cbd40c2b1cad2
AlainCartaya <alaincs40@gmail.com>
2023-11-15 19:13:14 -0500

    add printing methods

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -27,12 +27,28 @@
         :sch="sch"
         @update="school_data[selected_year].rooms = $event"
       />
+      <div class="full-width" v-if="printing">
+        <gschedule-component
+          v-for="group in sch.config.groupsOptions"
+          :key="`group-${group}`"
+          :sch="sch"
+          :school_data="school_data[selected_year].groups"
+          :group_keys="group_keys"
+          :year_keys="year_keys"
+          :selected_group="group"
+          :selected_year="selected_year"
+          :selected_color="selected_color"
+          @update-group="selected_group = $event"
+          :readonly="true"
+          class="full-height"
+        />
+      </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, nextTick } from 'vue';
 import GscheduleComponent from 'components/base/component/BaseGscheduleComponent.vue';
 import SscheduleComponent from 'components/base/component/BaseSscheduleComponent.vue';
 import { useScheduleTimetabling } from 'components/base/hooks/useTimetabling.hooks';
@@ -43,6 +59,8 @@ export default defineComponent({
   components: { GscheduleComponent, SscheduleComponent },
   setup() {
     const selected_color = ref('rgb(0,0,0)');
+    const printing = ref(false);
+
     const {
       sch,
       school_data,
@@ -57,9 +75,8 @@ export default defineComponent({
       onChangeYear,
       onUpdateBase,
       onExport,
-      onImport,
+      onImport
     } = useScheduleTimetabling();
-
     // const;
     return {
       sch,
@@ -69,6 +86,7 @@ export default defineComponent({
       selected_year,
       selected_group,
       selected_color,
+      printing,
       add_group,
       add_year,
       onSave,
@@ -78,24 +96,28 @@ export default defineComponent({
       onExport,
       onImport,
       onPrint() {
-        const toPrint = document.getElementById('printID')?.innerHTML;
-        let stylesHtml = '';
-        document
-          .querySelectorAll('link[rel="stylesheet"], style')
-          .forEach((value) => {
-            stylesHtml += value.outerHTML;
-          });
+        printing.value = true;
+        nextTick(() => {
+          const toPrint = document.getElementById('printID')?.innerHTML;
+          let stylesHtml = '';
+          document
+            .querySelectorAll('link[rel="stylesheet"], style')
+            .forEach((value) => {
+              stylesHtml += value.outerHTML;
+            });
 
-        // Open the print window
-        const WinPrint = window.open(
-          '',
-          '',
-          'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0'
-        );
+          // Open the print window
+          const WinPrint = window.open(
+            '',
+            '',
+            'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0'
+          );
 
-        WinPrint?.document.write(`<!DOCTYPE html>
+          WinPrint?.document.write(`<!DOCTYPE html>
 <html>
   <head>
+    <title> ${sch.config.scheduleName || 'Horario'} ${selected_year.value}
+    </title>
     ${stylesHtml}
   </head>
   <body>
@@ -103,9 +125,16 @@ export default defineComponent({
   </body>
 </html>`);
 
-        WinPrint?.document.close();
-        WinPrint?.focus();
-        WinPrint?.print();
+          WinPrint?.document.close();
+          WinPrint?.focus();
+          setTimeout(() => {
+            WinPrint?.print();
+            setTimeout(() => {
+              WinPrint?.close();
+              printing.value = false;
+            }, 200);
+          }, 200);
+        });
       }
     };
   }
